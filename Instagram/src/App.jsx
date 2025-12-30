369fa9924f238ba38fb0e50954646da9585ae0c2
pugalvanan <dheeranpugal7@gmail.com>
2025-12-30 11:10:11 +0530

    initial commit

---
+++ Instagram/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+function App() {
+  return (
+    <div className='d-flex vh-100 '>
+      <div className='w-25'><Sidebar/></div>
+      <div className='w-50 bg-info  '>feed</div>
+      <div className='w-30'>suggestions</div>
+    </div>
+  )
+}
+
+export default App
